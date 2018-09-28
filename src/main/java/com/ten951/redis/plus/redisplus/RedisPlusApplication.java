b5c3ea84353bc67391ff5650b40aceaf6862505a
王永天 <wangyongtian@yxqiche.com>
2018-09-28 11:03:33 +0800

    项目构建 Spring boot

---
+++ src/main/java/com/ten951/redis/plus/redisplus/RedisPlusApplication.java
@@ -0,0 +1,12 @@
+package com.ten951.redis.plus.redisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPlusApplication.class, args);
+    }
+}
